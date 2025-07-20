944b9121ed2af0edc74006646789a734b9dffbc2
Джони <magzon2@gmail.com>
2025-07-21 00:04:20 +0700

    upd: logic of my mini console app

--- src/MainManagment.java
+++ src/MainManagment.java
@@ -1,5 +1,39 @@
 public class MainManagment {
     public static void main(String[] args) {
+        EventDispatcher dispatcher = new EventDispatcher();
 
+        EventListener clickHandler = new EventListener() {
+            @Override
+            public void handle(Event event) {
+                System.out.println("The click was processed");
+                ((ClickEvent)event).click();
+            }
+        };
+
+
+        EventListener keyHandler = new EventListener() {
+            @Override
+            public void handle(Event event) {
+                System.out.println("The PressButton was processed");
+                ((KeyPressEvent)event).pressButton();
+            }
+        };
+
+        EventListener DownloadHandler  = new EventListener() {
+            @Override
+            public void handle(Event event) {
+                System.out.println("The Download was processed");
+                ((FileDownloadEvent)event).startDownload();
+            }
+        };
+
+
+        dispatcher.subscribeClick(clickHandler);
+        dispatcher.subscribeKeyPressed(keyHandler);
+        dispatcher.subscribeDownload(DownloadHandler);
+
+        dispatcher.dispatch(new ClickEvent());
+        dispatcher.dispatch(new KeyPressEvent());
+        dispatcher.dispatch(new FileDownloadEvent());
     }
 }
